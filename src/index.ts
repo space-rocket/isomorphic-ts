69371dee8f86a358d4ddc8a4c280556faf2da9c3
Michael Chavez <bigbassroller@gmail.com>
2016-03-28 09:30:06 -0700

    added nunjucks

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,51 @@
 /// <reference path="../typings/tsd.d.ts" />
 import * as Hapi from 'hapi';
+import * as nunjucks from 'nunjucks';
 
+// configure nunjucks to read from the dist directory
+nunjucks.configure('./dist');
+
+// create a server with a host and port
 const server = new Hapi.Server();
-server.connection({ 
-    host: 'localhost', 
-    port: 8000 
+server.connection({
+  host: 'localhost',
+  port: 8000
 });
 
+function getName(request) {
+  // default values
+  let name = {
+    fname: 'Michael',
+    lname: 'Chavez'
+  };
+  // split path params
+  let nameParts = request.params.name ? request.params.name.split('/') : [];
+
+  // order of precedence
+  // 1. path param
+  // 2. query param
+  // 3. default value
+  name.fname = (nameParts[0] || request.query.fname) ||
+    name.fname;
+  name.lname = (nameParts[1] || request.query.lname) ||
+    name.lname;
+
+  return name;
+}
+
+// add the route
 server.route({
-    method: 'GET',
-    path:'/hello/{name}', 
-    handler: function (request, reply) {
-
-        return reply('hello ' + request.params['name']);
-    }
+  method: 'GET',
+  path:'/hello/{name*}',
+  handler: function (request, reply) {
+    // read template and compile using context object
+    nunjucks.render('index.html', getName(request), function (err, html) {
+      // reply with HTML response
+      reply(html);
+    });
+  }
 });
 
-server.start((err) => {
-    if (err) { throw err; }
-    console.log('Server running at:', server.info.uri);
-});
+// start the server
+server.start();
 
